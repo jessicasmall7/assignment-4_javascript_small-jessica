52c08f7c9cd59d5b5ef7b52597d68cb2cf565c46
Jessica-Small <jlsmall@ufl.edu>
2018-06-18 21:21:38 -0400

    functions branch attempt :(

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function howdy(){
+  return 'Jessica Small says hello.';
+}
+
+console.log ( howdy );
+
+var numba = {
+  function (string){
+    if(string.length < 7) {
+      return 'What a short little word!';}
+    if(string.length > 7) {
+      return 'I’m sorry, but that’s too many to count.';}
+    if(string.length == 7) {
+      return '7, what a perfect choice!';}
+}}
+
+console.log ( ("7") );
+console.log ( ("seventy") );
+console.log ( ("university") );
+
+var combiner = function inception(display, favMovie){
+  display(favMovie);
+}
+
+var  message = inception('Elf', 'is a fantastic movie.');
+
+console.log ( inception );
